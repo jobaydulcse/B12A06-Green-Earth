1f6d00be0e7f9d5cebe343e1a458bfcf476472a0
jobaydulcse <jobaydul.cse@gmail.com>
2025-09-09 19:07:33 +0600

    categories API called

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ const manageLoader = (loadingStatus) => {
     plantCards.classList.remove("hidden");
   }
 };
+const loadAllCategories = async () => {
+  const url = "https://openapi.programming-hero.com/api/categories";
+  const res = await fetch(url);
+  const data = await res.json();
+  showAllCategories(data.categories);
+};
+
+const showAllCategories = (categories) => {
+  const categoryList = document.getElementById("category-list");
+  categories.forEach((category) => {
+    categoryList.innerHTML += `
+            <li id="tree-${category.id}" onclick="loadPlantsByCategory('${category.id}')" class="px-3 py-2 cursor-pointer rounded duration-300 hover:bg-[#15803d] hover:text-white">${category.category_name}</li>
+        `;
+  });
+};
